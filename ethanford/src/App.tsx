ce0e109a5d055fee6fb2763ed725e9f08af2a607
Ethan Ford <ethanfordj5@gmail.com>
2023-10-18 16:17:56 -0600

    :tada: launched development for da portweb

---
+++ ethanford/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-blue-500 text-center">
+          Hellow World
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
