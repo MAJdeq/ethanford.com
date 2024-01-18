694587e568a5a58e7e9320766c4609e52d6a0861
Ethan Ford <ethanfordj5@gmail.com>
2024-01-18 13:34:44 -0700

    added animations to project divs

--- src/components/sections/Project.tsx
+++ src/components/sections/Project.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 import { Carousel } from "react-responsive-carousel";
 
 interface ProjectProps {
@@ -16,14 +17,22 @@ export function Project({
   descriptions,
   images,
 }: ProjectProps) {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
   const isProjectIdEven = projectId % 2 === 0;
-  console.log(projectId);
-
   return (
-    <div className="flex rounded-lg container mx-auto bg-gray-800 p-4 w-3/4 mt-5">
+    <div
+      ref={ref}
+      className={`flex rounded-lg container mx-auto bg-gray-800 p-4 lg:w-3/4 mt-5 opacity-0 animate__animated ${
+        inView && isProjectIdEven
+          ? "opacity-100 animate__fadeInRight"
+          : "opacity-100 animate__fadeInLeft"
+      }`}
+    >
       {isProjectIdEven && (
         <>
-          <div className="text-white m-5 w-1/2">
+          <div className="text-white w-1/2">
             <p className="text-2xl"> {title} </p>
             <p className="text-lg text-gray-500"> {technologies} </p>
             <div>
@@ -36,7 +45,7 @@ export function Project({
           </div>
           <Carousel
             infiniteLoop={true}
-            className="text-center pl-7 pt-7 pr-7 w-1/2 border-r border-gray-700"
+            className="text-center pl-7 pt-7 pr-7 w-1/2 border-l border-gray-700"
             renderItem={(item, options) => (
               <div
                 className={`carousel-item ${
